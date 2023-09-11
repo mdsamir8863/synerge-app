0328792dffabf4661ddd062660ec246dd2bb09fd
vishal gehlot <vishalgehlot334@gmail.com>
2023-09-11 12:23:26 +0530

    restructure of dir. --fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,5 @@
-import React from "react";
-import { Router, Route, Routes } from "react-router-dom";
-import Signup from "./screens/startup/Signup";
-import Login from "./screens/startup/Login";
-import ForgetPassword from "./screens/startup/ForgetPassword";
-import DashMain from "./app/components/DashMain/DashMain";
-import Navbar from "./app/components/sidebar/Navbar";
-import Feeds from "./app/components/dashFeed/Feeds";
-import DashBoard from "./screens/home/DashBoard/DashBoard";
-const App = () => {
-  return (
-    <div className="">
-      {/* <Navbar /> */}
-      {/* <DashMain /> */}
-      {/* <Feeds /> */}
-      <DashBoard />
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/forget" element={<ForgetPassword />} />
-      </Routes>
-    </div>
-  );
-};
+import Routing from "./Routing";
 
-export default App;
+const App = () => <Routing/>
+
+export default App
---
+++ src/Routing.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Routing = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Routing;
